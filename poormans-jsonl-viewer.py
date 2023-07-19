3d6b624d6c1520de18a6d4b009ae85f6d09f0f23
Stefaan Lippens <soxofaan@gmail.com>
2023-07-19 14:54:51 +0200

    Add logging of some basic stats

--- poormans-jsonl-viewer.py
+++ poormans-jsonl-viewer.py
@@ -1,25 +1,34 @@
+import collections
 import json
 import logging
 import sys
-
 
 _log = logging.getLogger("poormans-jsonl-viewer")
 
 
 def main():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.INFO)
     fields = sys.argv[1:]
+    stats = collections.Counter()
     if not fields:
         raise RuntimeError("Specify which fields to render.")
-    for line in sys.stdin:
-        if not line.strip():
-            continue
-        try:
-            data = json.loads(line)
-        except Exception as e:
-            _log.warn(f"Failed to parse {line}: {e}")
-        values = [data.get(f) for f in fields]
-        print("\t".join(repr(v) for v in values))
+    try:
+        for line in sys.stdin:
+            stats["lines read"] += 1
+            if not line.strip():
+                continue
+            try:
+                data = json.loads(line)
+            except Exception as e:
+                _log.warn(f"Failed to parse {line!r}: {e}")
+                stats["parse errors"] += 1
+                continue
+            finally:
+                values = [data.get(f) for f in fields]
+                print("\t".join(repr(v) for v in values))
+                stats["entries shown"] += 1
+    finally:
+        _log.info(f"Stats: {stats}")
 
 
 if __name__ == "__main__":
